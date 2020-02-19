94d82b6f8993768a9ac130dc8dd7db7a4330907d
Abby Gottlich <abby.gottlich@gmail.com>
2020-02-19 15:49:54 -0600

    revised app modules and added components to root

--- grocery-list-recipe-book/src/app/app.module.ts
+++ grocery-list-recipe-book/src/app/app.module.ts
@@ -1,38 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { ProjectRootComponent } from './project-root/project-root.component';
-import { HeaderComponent } from './header/header.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeListComponent } from './recipe-list/recipe-list.component';
-import { ShoppingListEditComponent } from './shopping-list-edit/shopping-list-edit.component';
-import { IngredientComponent } from './ingredient/ingredient.component';
-import { RecipeItemComponent } from './recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
-import { RecipeComponent } from './recipe/recipe.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
+import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
+import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
+import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
+import { RecipesComponent } from "./recipes/recipes.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProjectRootComponent,
     HeaderComponent,
     ShoppingListComponent,
     RecipeListComponent,
-    ShoppingListEditComponent,
-    IngredientComponent,
     RecipeItemComponent,
     RecipeDetailComponent,
-    RecipeComponent,
     RecipesComponent,
     ShoppingEditComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
