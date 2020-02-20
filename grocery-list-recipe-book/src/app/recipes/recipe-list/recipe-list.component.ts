6d7809cbd5b946fc171712749b2544e7ea7aa4dd
Abby Gottlich <abby.gottlich@gmail.com>
2020-02-20 15:28:15 -0600

    added some buttons, and inputs

--- grocery-list-recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ grocery-list-recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from "../recipe.model";
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      "A Test Recipe",
+      "This is simply a test.",
+      "https://assets3.thrillist.com/v1/image/2848840/size/gn-gift_guide_variable_c.jpg"
+    ),
+    new Recipe(
+      "A Test Recipe",
+      "This is simply a test.",
+      "https://assets3.thrillist.com/v1/image/2848840/size/gn-gift_guide_variable_c.jpg"
+    )
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
