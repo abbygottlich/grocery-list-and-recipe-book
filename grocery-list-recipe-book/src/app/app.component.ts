da633fcdabc10fcf2d18b6fef179afd3f92f63b6
Abby Gottlich <abby.gottlich@gmail.com>
2020-02-26 14:14:37 -0600

    observable written

--- grocery-list-recipe-book/src/app/app.component.ts
+++ grocery-list-recipe-book/src/app/app.component.ts
@@ -1,8 +1,50 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import { interval, Subscription, Observable } from "rxjs";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {}
+export class AppComponent implements OnInit, OnDestroy {
+  private firstObsSubscription: Subscription;
+
+  constructor() {}
+
+  ngOnInit() {
+    // this.firstObsSubscription = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // });
+
+    const customIntervalObservable = Observable.create(observer => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if (count == 2) {
+          observer.complete();
+        }
+        if (count > 3) {
+          observer.error(new Error("Count is greater than 3!"));
+        }
+        count++;
+      }, 1000);
+    });
+
+    this.firstObsSubscription = customIntervalObservable.subscribe(
+      data => {
+        console.log(data);
+      },
+      error => {
+        console.log(error);
+        alert(error.message);
+      },
+      () => {
+        console.log("Completed!");
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.firstObsSubscription.unsubscribe();
+  }
+}
